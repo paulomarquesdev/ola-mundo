917757769242a09ee1495c857ca805db232b7259
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-23 00:39:12 -0300

    Correção

--- src/components/Banner/style.js
+++ src/components/Banner/style.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
     color: var(--white);
     display: grid;
     grid-template-columns: 1fr 1fr;
+    align-items: center;
 
     h1 {
         font-family: var(--fontSecondary);
