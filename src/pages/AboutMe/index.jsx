d47aaedd3b71f02c8160624028f1dbfa05bd3874
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-23 03:27:13 -0300

    Implementação - Página Sobre Mim

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { PostModel } from 'components/PostModel';
+import { Container } from './style';
+import imgCapa from '../../assets/images/sobre_mim_capa.png';
 
 export function AboutMe() {
   return (
-    <Outlet />
-    // <Container>
-    //   <Banner />
+    <React.Fragment>
+      <Outlet />
 
-    // </Container>
+      <PostModel
+        imgCapa={imgCapa}
+        title="Sobre Mim"
+      >
+        <Container>
+          <h3>Olá, eu sou o Paulo!</h3>
+          <img src="https://github.com/paulomarquesdev.png" alt="Foto do Paulo Marques Dev" />
+          <p>
+            Oi, tudo bem? Eu sou estudante e desenvolvedor Front-end e estou feliz de te ver por aqui.
+          </p>
+          <p>
+            Minha história com programação começou no Centro Universitário do Distrito Federal (UDF), onde fiz um semestre em Sistemas de Informação. Eu aprendi lógica de programação e o básico de algumas linguagens, como HTML, CSS, Python, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época por questões financeiras não pude seguir com a formação.
+          </p>
+          <p>
+            Nesse ano de 2022, pude investir em uma plataforma de ensino focado no mercado de tecnologia, a Alura. Desde então, estou estudando tecnologias de Front-End. Durante os últimos meses estudei criação de páginas web utilizando HTML, CSS e JavaScript puro (Vanilla). Atualmente, estou estudando ReactJS, Next.js e TypeScript.
+          </p>
+          <p>
+            Desde o começo de minha jornada, venho buscando evoluir, aplicando para vagas de front-end lutando por minha primeira oportunidade na área.
+          </p>
+        </Container>
+      </PostModel>
+    </React.Fragment>
   )
 }
--- src/pages/AboutMe/style.js
+++ src/pages/AboutMe/style.js
@@ -1,5 +1,52 @@
 import styled from "styled-components";
 
-export const Container = styled.main`
+export const Container = styled.div`
+    img {
+        margin-bottom: .5rem;
+        margin-left: 1.5rem;
+        width: 40vw;
+        float: right;
+    }
 
+    h3 {
+        margin-bottom: 2rem;
+        font-size: 2.5rem;
+    }
+
+    p {
+        text-align: justify;
+        font-size: 1.5rem;
+        line-height: 2.25rem;
+        margin-bottom: 1.5rem;
+    }
+
+    @media (max-width: 1100px) {
+        img {
+            float: none;
+            display: block;
+            margin: 0 auto 2rem;
+            width: 70vw;
+        }
+
+        p {
+            margin-bottom: 2rem;
+        }
+    }
+
+    @media (max-width: 744px) {
+        img {
+            width: 100%;
+        }
+
+        h3 {
+            font-size: 1.5rem;
+            line-height: 2rem;
+            margin-bottom: 1.5rem;
+        }
+
+        p {
+            font-size: 1rem;
+            line-height: 1.75rem;
+        }
+    }
 `;
