c7fc4f34e9bdcba9eed0e5ab66454d5c525aee6d
Paulo Marques <paulomarques.rs1@gmail.com>
2022-11-23 00:42:22 -0300

    Correção

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Container } from './style';
-import circleColorImg from '../../assets/circulo_colorido.png';
+import circleColorImg from '../../assets/images/circulo_colorido.png';
 
 export function Banner() {
     return (
         <Container>
             <div className='presentation'>
                 <h1>Olá, mundo!</h1>
-                <p>Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)</p>
+                <p>Boas vindas ao meu espaço pessoal! Eu sou Paulo Marques, estudante e Desenvolvedor Front-end. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)</p>
             </div>
             <div className='images'>
                 <img className='circleColor' src={circleColorImg} alt="Imagem de circulo colorido" aria-hidden={true} />
